a7053babb19e94309507d68b9b07b223cbe1d39b
Om Patel <omipatel7113@gmail.com>
2025-03-24 17:41:38 +0530

    kotlin project setup

---
+++ src/main/kotlin/com/ipl/ipl/IplApplication.kt
@@ -0,0 +1,11 @@
+package com.ipl.ipl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IplApplication
+
+fun main(args: Array<String>) {
+	runApplication<IplApplication>(*args)
+}
